45884f4ba7ffb4b3a6a24d9b712f8c0e568cd951
Rafael Henrique da Silva <rafael.silva@hbsis.com.br>
2019-11-25 16:42:40 -0300

    correção para cnpj unico

--- src/main/resources/db/migration/V20191122163925__seg_fornecedor.sql
+++ src/main/resources/db/migration/V20191122163925__seg_fornecedor.sql
@@ -1,9 +1,9 @@
 create table seg_fornecedores
 (
     id    BIGINT IDENTITY (1, 1) NOT NULL,
-    razao_social VARCHAR(100)     NOT NULL,
-    cnpj VARCHAR (14)            NOT NULL,
-    nome_fantasia VARCHAR(100)    NOT NULL,
+    razao_social VARCHAR(100)    NOT NULL,
+    cnpj VARCHAR (14)            UNIQUE NOT NULL,
+    nome_fantasia VARCHAR(100)   NOT NULL,
     endereco VARCHAR (100)       NOT NULL,
     telefone VARCHAR (12)        NOT NULL,
     email VARCHAR (100)          NOT NULL,
